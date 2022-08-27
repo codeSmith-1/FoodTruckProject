0005037812dcd87b10ab4cf09e73b734dc1fbcff
Tate Smith <thsmith.cofc@gmail.com>
2022-08-27 13:38:34 -0400

    menu start

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,10 +1,71 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Scanner;
+
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
+
+		boolean addTrucks = true;
+		int maxTrucks = 5;
+		int numOfTrucks = 0;
+		
+		FoodTruck[] trucks = new FoodTruck[maxTrucks];
+
+		for (int i = 0; i < maxTrucks; i++) {
+			FoodTruck truck = new FoodTruck();
+
+			System.out.println("Name of truck: ");
+			String input = kb.next();
+			if (input.equalsIgnoreCase("quit")) {
+				break;
+			} else {
+				truck.setTruckName(input);
+			}
+
+			System.out.println("Food type: ");
+			truck.setFoodType(kb.next());
+
+			System.out.println("Your rating: ");
+			truck.setRating(kb.nextInt());
+
+			truck.setTruckId(i);
+
+			trucks[i] = truck;
+			System.out.println(truck.getTruckID());
+	
+			numOfTrucks++;
+		}
+
+		System.out.println();
+		boolean start = true;
+
+		while (start) {
+			System.out.println("_______MENU______");
+			System.out.println("1) See list of food trucks");
+			System.out.println("2) See average rating of food trucks");
+			System.out.println("3) Display highest rated food truck");
+			System.out.println("4) Quit");
+
+			System.out.print("Selet an option:");
+			int userSelect = kb.nextInt();
+
+			switch (userSelect) {
+			case 1:
+				truck.getTrucks(trucks, numOfTrucks);
+				break;
+
+			default:
+				break;
+			}
+		}
 
 	}
 
 }
+
+// create array of foodtrucks
+// if statements
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,5 +1,54 @@
 package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
+	private int truckId = 1;
+	private String truckName;
+	private String foodType;
+	private int rating;
+
+//id is auto assigned from a 'static' field that is incremented as each truck is created
+
+	public void getTrucks(FoodTruck[] array, int numOfTrucks) {
+		for (int i = 0; i < numOfTrucks; i++) {
+			System.out.println(array[i].toString());
+		}
+	}
+
+	public String toString() {
+
+		return "Name: " + truckName + " Food: " + foodType + " Rating: " + rating + " Truck ID: " + truckId;
+	}
+
+	public int getTruckID() {
+		return truckId;
+	}
+
+	public void setTruckId(int num) {
+		this.truckId += num;
+	}
+
+	public String getTruckName() {
+		return truckName;
+	}
+
+	public void setTruckName(String truckName) {
+		this.truckName = truckName;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
 
 }
